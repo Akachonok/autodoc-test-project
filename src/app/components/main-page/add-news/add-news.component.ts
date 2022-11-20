651985ce227fdf48e24a76e0dfb3357546ba7b05
Evgeniy Akachonok <akachonok@itspartner.net>
2022-11-20 23:04:24 +0300

    add modal

---
+++ src/app/components/main-page/add-news/add-news.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-news',
+  templateUrl: './add-news.component.html',
+  styleUrls: ['./add-news.component.scss'],
+})
+export class AddNewsComponent implements OnInit {
+  form: FormGroup;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: unknown) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  handle(event: any): void {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(event.files[0]);
+    reader.onload = (value) => {
+      this.form.controls['file'].setValue(value.target?.result);
+    };
+  }
+
+  initForm(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+      file: new FormControl(null, Validators.required),
+    });
+  }
+}
